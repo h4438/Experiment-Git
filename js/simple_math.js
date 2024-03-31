86e8d4905a32496f1fea74575738a506531342e6
h4438 <www.hao2912@gmail.com>
2024-03-31 15:36:52 +0700

    add subtraction function

--- js/simple_math.js
+++ js/simple_math.js
@@ -2,8 +2,15 @@ function add_number(a, b){
     return a + b
 }
 
+
+function subtract_number(a, b){
+    return a - b
+}
+
 const main = () => {
     let c = add_number(1, 3)
+    console.log(c)
+    c = subtract_number(5, 21)
     console.log(c)
 }
 
