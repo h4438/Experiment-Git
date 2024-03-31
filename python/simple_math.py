f669824adb2b49cb31faaeffef231afac5090251
Trung Hieu <99268647+h4438@users.noreply.github.com>
2024-03-31 14:44:20 +0700

    add arrays addition function

--- python/simple_math.py
+++ python/simple_math.py
@@ -9,9 +9,21 @@ def add_array(array: List[float], number: int):
     """
     return [i+number for i in array]
 
-
+def add_arrays(array_a: List[float], array_b: List[float]):
+    """
+        Add a number to each element in the array
+        @param array_a a list of numbers
+        @param array_b a list of numbers
+        
+        @return an array
+    """
+    return [i+y for i, y in zip(array_a, array_b)]
+    
+    
 if __name__ == "__main__":
 
     a = [1, 2, 3, 4, 5]
     b = add_array(array=a, number=10)
     print(b)
+    c = add_array(array_a=a, array_b=b)
+    print(c)
