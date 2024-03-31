6400d255acf7b3e2b3967f243072dadadfeb7182
h4438 <www.hao2912@gmail.com>
2024-03-31 14:39:26 +0700

    add array subtraction function

--- python/simple_math.py
+++ python/simple_math.py
@@ -9,9 +9,21 @@ def add_array(array: List[float], number: int):
     """
     return [i+number for i in array]
 
+def subtract_array(array: List[float], number:int):
+    """
+        Add a number to each element in the array
+        @param array a list of numbers
+        @param number a number to subtract
+        
+        @return an array
+    """
+    return [i-number for i in array]
+
 
 if __name__ == "__main__":
 
     a = [1, 2, 3, 4, 5]
     b = add_array(array=a, number=10)
+    print(b)
+    b = subtract_array(array=a, number=5)
     print(b)
